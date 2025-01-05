86bc21a046c7928162f7ae55696103d8d9f9cb5e
lllmoseslll <mosespeter620@gmail.com>
2025-01-05 15:49:26 +0300

    displaying meals

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,9 @@ import { CATEGORIES } from "../data/dummy-data";
 const CategoriesScreen = ({ navigation }) => {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate("Meals Overview");
+      navigation.navigate("Meals Overview", {
+        categoryId: itemData.item.id,
+      });
     }
 
     return (
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MealItem from '../component/MealItem';
+import { MEALS } from '../data/dummy-data';
 
-const MealsOverviewScreen = () => {
+const MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  })
+
+  function renderMealItem(itemData) {
+    return (
+      <MealItem title={itemData.item.title} />
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>MealsOverviewScreen</Text>
+      <FlatList data={displayedMeals} keyExtractor={(item) => item.id}
+        renderItem={renderMealItem } />
     </View>
   )
 }
