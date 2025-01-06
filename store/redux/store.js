b71258b5b2cf54f3a82f1d67d880aec087b9f81d
lllmoseslll <mosespeter620@gmail.com>
2025-01-06 22:38:55 +0300

    managinag redux state and dispatching actions

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  favoritesRudecer  from "./favorites";
 
 export const store = configureStore({
-  reducer: {},
+    reducer: {
+      favoriteMeals: favoritesRudecer
+  },
 });
